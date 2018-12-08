a14df0c5b8244ae0cd1a4c75b96c8208265f9843
Noyon <noyonict@gmail.com>
2018-12-08 18:24:49 +0600

    index page design finalize

--- Image_Matching/image/views.py
+++ Image_Matching/image/views.py
@@ -4,13 +4,16 @@ from .models import UploadImage
 
 # Create your views here.
 def index(request):
+    images = UploadImage.objects.order_by('-upload_at')
+    new_image = None
     if request.method == 'POST':
         image_file = request.FILES['uploaded_image']
-        UploadImage.objects.create(image_link=image_file)
-    images = UploadImage.objects.order_by('-upload_at')
+        new_image = UploadImage.objects.create(image_link=image_file)
+        images = images.exclude(id=new_image.id)
     context = {
         'title': 'Image Matching Portal',
         'images': images,
+        'new_image': new_image,
     }
     return render(request, 'image/index.html', context)
 
